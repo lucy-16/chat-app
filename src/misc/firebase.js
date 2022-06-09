c1582ac757739779aadc89d4fff266ce92bbcec9
Lucy Varshitha <lucyvarshithabellam@gmail.com>
2022-06-09 13:26:25 +0530

    initialized firebase project

---
+++ src/misc/firebase.js
@@ -0,0 +1,14 @@
+
+import firebase from 'firebase/app';
+
+const config = {
+    apiKey: "AIzaSyAJhMoqrBnQEuTeNnRYOVa-tv0cz_UR1T8",
+    authDomain: "chat-app-web-5b117.firebaseapp.com",
+    databaseURL: "https://chat-app-web-5b117-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "chat-app-web-5b117",
+    storageBucket: "chat-app-web-5b117.appspot.com",
+    messagingSenderId: "835886449071",
+    appId: "1:835886449071:web:9cbb669293303c5e6daf5a"
+  };
+
+const app = firebase.initializeApp(config);
