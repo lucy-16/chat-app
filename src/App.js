a6a19a8d2a7f99a3120fa309ccaa8ddf768afd41
Lucy Varshitha <lucyvarshithabellam@gmail.com>
2022-06-09 19:33:12 +0530

    manage user with profile context; put real time subs

--- src/App.js
+++ src/App.js
@@ -6,19 +6,23 @@ import PrivateRoute from "./components/PrivateRoute";
 import SignIn from "./pages/SignIn";
 import Home from "./pages/Home";
 import PublicRoute from "./components/PublicRoute";
+import { ProfileProvider } from './context/profile.context';
+
 
 
 
 function App() {
   return (
-    <Switch>
-      <PublicRoute path="/signin">
-        <SignIn />
-      </PublicRoute>
-      <PrivateRoute path="/">
-        <Home />
-      </PrivateRoute>
-    </Switch>
+    <ProfileProvider>
+<Switch>
+  <PublicRoute path="/signin">
+    <SignIn />
+  </PublicRoute>
+  <PrivateRoute path="/">
+    <Home />
+  </PrivateRoute>
+</Switch>
+</ProfileProvider>
   );
 }
 
