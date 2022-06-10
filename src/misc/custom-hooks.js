52656a8b9d278448ab2cd48b66eaa2fc9eecb6cc
Lucy Varshitha <lucyvarshithabellam@gmail.com>
2022-06-10 16:51:10 +0530

    display real-time presence

--- src/misc/custom-hooks.js
+++ src/misc/custom-hooks.js
@@ -1,4 +1,5 @@
 import { useState, useCallback, useEffect  } from 'react';
+import { database } from './firebase';
 
 export function useModalState(defaultValue = false) {
   const [isOpen, setIsOpen] = useState(defaultValue);
@@ -16,12 +17,32 @@ export const useMediaQuery = query => {
     useEffect(() => {
       const queryList = window.matchMedia(query);
       setMatches(queryList.matches);
-  
       const listener = evt => setMatches(evt.matches);
-  
       queryList.addListener(listener);
       return () => queryList.removeListener(listener);
     }, [query]);
   
     return matches;
   };
+
+  export function usePresence(uid) {
+    const [presence, setPresence] = useState(null);
+  
+    useEffect(() => {
+      const userStatusRef = database.ref(`/status/${uid}`);
+  
+      userStatusRef.on('value', snap => {
+        if (snap.exists()) {
+          const data = snap.val();
+  
+          setPresence(data);
+        }
+      });
+  
+      return () => {
+        userStatusRef.off();
+      };
+    }, [uid]);
+  
+    return presence;
+  }
