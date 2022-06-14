58913a77c8c09a5b6235369bcff16a3749d78b63
Lucy Varshitha <lucyvarshithabellam@gmail.com>
2022-06-14 19:49:23 +0530

    handle delete message operation

--- src/components/chat-window/messages/MessageItem.js
+++ src/components/chat-window/messages/MessageItem.js
@@ -9,7 +9,7 @@ import { auth } from '../../../misc/firebase';
 import { useHover, useMediaQuery} from '../../../misc/custom-hooks';
 import IconBtnControl from './IconBtnControl';
 
-const MessageItem = ({ message, handleAdmin, handleLike }) => {
+const MessageItem = ({ message, handleAdmin, handleLike, handleDelete }) => {
   const { author, createdAt, text, likes, likeCount } = message;
 
   const [selfRef, isHovered] = useHover();
@@ -67,7 +67,14 @@ const MessageItem = ({ message, handleAdmin, handleLike }) => {
         />
       </div>
 
-     
+      {isAuthor && (
+          <IconBtnControl
+            isVisible={canShowIcons}
+            iconName="close"
+            tooltip="Delete this message"
+            onClick={() => handleDelete(message.id)}
+          />
+        )}   
       <div>
         <span className="word-breal-all">{text}</span>
       </div>
