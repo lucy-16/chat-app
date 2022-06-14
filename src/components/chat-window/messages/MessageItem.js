509e83d1d643788b3d1e91a71feeb95c060e359a
Lucy Varshitha <lucyvarshithabellam@gmail.com>
2022-06-14 20:04:52 +0530

    display uploaded files

--- src/components/chat-window/messages/MessageItem.js
+++ src/components/chat-window/messages/MessageItem.js
@@ -8,9 +8,21 @@ import { useCurrentRoom } from '../../../context/currentroom.context';
 import { auth } from '../../../misc/firebase';
 import { useHover, useMediaQuery} from '../../../misc/custom-hooks';
 import IconBtnControl from './IconBtnControl';
+import ImgBtnModal from './ImgBtnModal';
 
+const renderFileMessage = file => {
+  if (file.contentType.includes('image')) {
+    return (
+      <div className="height-220">
+        <ImgBtnModal src={file.url} fileName={file.name} />
+      </div>
+    );
+  }
+
+  return <a href={file.url}>Download {file.name}</a>;
+};
 const MessageItem = ({ message, handleAdmin, handleLike, handleDelete }) => {
-  const { author, createdAt, text, likes, likeCount } = message;
+  const { author, createdAt, text, file, likes, likeCount } = message;
 
   const [selfRef, isHovered] = useHover();
   const isMobile = useMediaQuery('(max-width: 992px)');
@@ -75,8 +87,10 @@ const MessageItem = ({ message, handleAdmin, handleLike, handleDelete }) => {
             onClick={() => handleDelete(message.id)}
           />
         )}   
+        
       <div>
-        <span className="word-breal-all">{text}</span>
+      {text && <span className="word-breal-all">{text}</span>}
+        {file && renderFileMessage(file)}
       </div>
     </li>
   );
