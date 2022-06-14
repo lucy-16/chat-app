89b1507220ec550e8f228bc053368baab21d1226
Lucy Varshitha <lucyvarshithabellam@gmail.com>
2022-06-14 20:31:37 +0530

    grouped messages by dates

--- src/misc/helpers.js
+++ src/misc/helpers.js
@@ -49,3 +49,17 @@ export function getNameInitials(name) {
   
     return updates;
   }
+
+  export function groupBy(array, groupingKeyFn) {
+    return array.reduce((result, item) => {
+      const groupingKey = groupingKeyFn(item);
+  
+      if (!result[groupingKey]) {
+        result[groupingKey] = [];
+      }
+  
+      result[groupingKey].push(item);
+  
+      return result;
+    }, {});
+  }
